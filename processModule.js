42dc2664803f652ab88d502157ebe18b66f295d8
sngom1995 <ngomsamba75@gmail.com>
2023-09-19 09:02:15 +0000

    process module

---
+++ processModule.js
@@ -0,0 +1,13 @@
+console.log('This is the first line.');
+
+process.on('beforeExit', (code) => {
+    console.log('Process beforeExit event with code: ', code);
+}
+);
+
+process.on('exit', (code) => {
+    console.log('Process exit event with code: ', code);
+}
+);
+
+process.exit(1);
